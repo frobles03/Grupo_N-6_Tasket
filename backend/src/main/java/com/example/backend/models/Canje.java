54a2b15c7d88c68b006e2a20471ff87d87cc1203
alex2022-lab <111999591+alex2022-lab@users.noreply.github.com>
2024-06-29 17:53:32 -0400

    Eliminación
    Se eliminó la carpeta bin
    
    Había duplicación de código en la carpeta bin y se tuvo que eliminar para solucionarlo

--- backend/src/main/java/com/example/backend/models/Canje.java
+++ backend/src/main/java/com/example/backend/models/Canje.java
@@ -8,6 +8,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 
+
 @Entity
 @Table(name = "canjes")
 public class Canje {
@@ -24,6 +25,51 @@ public class Canje {
 
     @Column(name = "descripcion")
     private String descripcion;
+
+    public Canje(){
+        
+    }
+
+    public Canje(Long id, int puntos, String titulo, String descripcion) {
+        this.id = id;
+        this.puntos = puntos;
+        this.titulo = titulo;
+        this.descripcion = descripcion;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public int getPuntos() {
+        return puntos;
+    }
+
+    public void setPuntos(int puntos) {
+        this.puntos = puntos;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
     
-    
+
+
+
 }
