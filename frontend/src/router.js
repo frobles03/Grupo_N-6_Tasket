93fc0aa6973cd1687b6255b5984896edd40c4c5b
francis <114947480+francisSM@users.noreply.github.com>
2024-07-02 12:11:37 -0400

    Update router.js

--- frontend/src/router.js
+++ frontend/src/router.js
@@ -4,7 +4,11 @@ import TareasCanjesAdmin from './components/admin/TareasCanjesAdmin.vue'
 import Login from './components/auth/Login.vue'
 import Register from './components/auth/Register.vue'
 import TiendaCanje from './components/user/TiendaCanje.vue'
-import Home from './components/home.vue';
+import Home from './components/home.vue'
+import AdminUsuarios from './components/admin/AdminUsuarios.vue'
+import AdminEstadisticas from './components/admin/AdminEstadisticas.vue'
+import AdminRoles from './components/admin/AdminRoles.vue'
+import AdminReportes from './components/admin/AdminReportes.vue'
 
 const routes = [
   { path: '/', component: Home },
@@ -12,7 +16,11 @@ const routes = [
   { path: '/login', component: Login },
   { path: '/tareas-canjes-admin', component: TareasCanjesAdmin },
   { path: '/register', component: Register },
-  { path: '/TiendaCanje', component: TiendaCanje },
+  { path: '/tienda-canje', component: TiendaCanje },
+  { path: '/admin-usuarios', component: AdminUsuarios },
+  { path: '/admin-estadisticas', component: AdminEstadisticas },
+  { path: '/admin-roles', component: AdminRoles },
+  { path: '/admin-reportes', component: AdminReportes }
 ]
 
 const router = createRouter({
